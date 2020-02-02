5bd6f678a308ff0bc674b4073cf2a5525194c962
Jon Jagger <jon@jaggersoft.com>
2020-02-02 18:34:51 +0000

    Added check_version.sh

---
+++ check_version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -Eeu
+
+readonly REGEX="image_name\": \"(.*)\""
+readonly JSON=`cat docker/image_name.json`
+[[ ${JSON} =~ ${REGEX} ]]
+readonly IMAGE_NAME="${BASH_REMATCH[1]}"
+
+readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly EXPECTED=0.5.0
+readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'zig version')
+
+if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
+  echo "VERSION CONFIRMED as ${EXPECTED}"
+else
+  echo "VERSION EXPECTED: ${EXPECTED}"
+  echo "VERSION   ACTUAL: ${ACTUAL}"
+  exit 42
+fi
